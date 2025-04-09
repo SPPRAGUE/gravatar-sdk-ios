1dd0990898a3e2dcd2d69a994e304d003c1d0246
Pinar Olguc <pinarolguc@gmail.com>
2025-04-09 16:05:45 +0100

    Generate the new openapi models

---
+++ Sources/Gravatar/OpenApi/Generated/UpdateProfileRequest.swift
@@ -0,0 +1,61 @@
+import Foundation
+
+/// The subset of data available for update. Field names match the ones in `Profile`. Only the provided fields will be updated.
+///
+package struct UpdateProfileRequest: Codable, Hashable, Sendable {
+    /// The user's display name. This is the name that is displayed on their profile.
+    package private(set) var displayName: String?
+    /// The about section on a user's profile.
+    package private(set) var description: String?
+    /// The phonetic pronunciation of the user's name.
+    package private(set) var pronunciation: String?
+    /// The pronouns the user uses.
+    package private(set) var pronouns: String?
+    /// The user's location.
+    package private(set) var location: String?
+    /// The user's job title.
+    package private(set) var jobTitle: String?
+    /// The user's current company's name.
+    package private(set) var company: String?
+
+    package init(
+        displayName: String? = nil,
+        description: String? = nil,
+        pronunciation: String? = nil,
+        pronouns: String? = nil,
+        location: String? = nil,
+        jobTitle: String? = nil,
+        company: String? = nil
+    ) {
+        self.displayName = displayName
+        self.description = description
+        self.pronunciation = pronunciation
+        self.pronouns = pronouns
+        self.location = location
+        self.jobTitle = jobTitle
+        self.company = company
+    }
+
+    enum CodingKeys: String, CodingKey, CaseIterable {
+        case displayName = "display_name"
+        case description
+        case pronunciation
+        case pronouns
+        case location
+        case jobTitle = "job_title"
+        case company
+    }
+
+    // Encodable protocol methods
+
+    package func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encodeIfPresent(displayName, forKey: .displayName)
+        try container.encodeIfPresent(description, forKey: .description)
+        try container.encodeIfPresent(pronunciation, forKey: .pronunciation)
+        try container.encodeIfPresent(pronouns, forKey: .pronouns)
+        try container.encodeIfPresent(location, forKey: .location)
+        try container.encodeIfPresent(jobTitle, forKey: .jobTitle)
+        try container.encodeIfPresent(company, forKey: .company)
+    }
+}
